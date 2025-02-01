70e0cdd00fdf131bccabe22f79688b667dfa4922
chirag <chiragchauhanx@gmail.com>
2025-02-01 13:44:06 +0530

    initial setup

---
+++ src/app/experts/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Experts() {
+  return (
+    <div>
+      Experts page
+      <Button>
+        hello
+      </Button>
+    </div>
+  );
+}
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Navbar(){
+    return (
+        <div className="flex w-full h-16 items-center justify-center ">
+            this is Navbar
+        </div>
+    )
+}
