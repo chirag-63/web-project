2bb81873e76720396797d6984099c97a666a359c
chirag <chiragchauhanx@gmail.com>
2025-02-03 01:38:56 +0530

    added configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["aceternity.com", "images.unsplash.com", "i.pinimg.com"],
+  },
+};
 
 export default nextConfig;
